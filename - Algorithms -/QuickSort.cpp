38bca1033ee0ca2cb89a6e913692e97b8776033b
MossFrog <kaanozcelik@sabanciuniv.edu>
2016-04-02 02:34:17 +0200

    Updated the CPP-Bundle to have an algorithms branch + added the recursive quicksort sample.

---
+++ - Algorithms -/QuickSort.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void quickSort(vector<int> & unsortedVector);
+
+int main()
+{
+	vector<int> mainVector;
+	mainVector.push_back(3);
+	mainVector.push_back(5);
+	mainVector.push_back(1);
+	mainVector.push_back(10);
+	mainVector.push_back(1);
+
+	quickSort(mainVector);
+	for (int i = 0; i <= mainVector.size() - 1; i++)
+	{
+		cout << mainVector[i] << endl;
+	}
+
+	cin.get();
+	return 0;
+}
+
+void quickSort(vector<int> & unsortedVector)
+{
+	//-- Subvector - 1 and Subvector - 2 are the partitions --//
+	vector<int> subVector_1;
+	vector<int> subVector_2;
+
+	vector<int> resultVector;
+
+	for (int i = 0; i < unsortedVector.size() - 1; i++)
+	{
+		if (unsortedVector[i] <= unsortedVector[unsortedVector.size() - 1])
+		{
+			subVector_1.push_back(unsortedVector[i]);
+		}
+
+		else
+		{
+			subVector_2.push_back(unsortedVector[i]);
+		}
+	}
+
+
+	//-- Recursively call the method depending on the size of the Subvectors --//
+
+	if (subVector_1.size() > 1)
+	{
+		quickSort(subVector_1);
+	}
+
+	if (subVector_2.size() > 1)
+	{
+		quickSort(subVector_2);
+	}
+
+	for (int i = 0; i < subVector_1.size(); i++)
+	{
+		resultVector.push_back(subVector_1[i]);
+	}
+
+	resultVector.push_back(unsortedVector[unsortedVector.size() - 1]);
+
+	for (int i = 0; i < subVector_2.size(); i++)
+	{
+		resultVector.push_back(subVector_2[i]);
+	}
+
+	unsortedVector = resultVector;
+}
